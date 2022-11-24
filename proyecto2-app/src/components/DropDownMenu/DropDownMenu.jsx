047bc7561222e196c77ccefc069ac8557546f40a
Marco Flores <hfmarcoalexander@hotmail.com>
2022-11-23 20:53:51 -0500

    PreEntrega1-HuamanFlores

--- proyecto2-app/src/components/DropDownMenu/DropDownMenu.jsx
+++ proyecto2-app/src/components/DropDownMenu/DropDownMenu.jsx
@@ -5,9 +5,10 @@ export default function Desplegable() {
         <li className='dropdown'>
             <a href="" className='dropbtn'>CATEGORIAS</a>
             <div className='dropdown-content'>
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
+            <a href="#"><img src='/public/img/shooter.png'></img>Link 1</a>
+            <a href="#"><img src='/public/img/jigsaw.png'></img>Link 2</a>
+            <a href="#"><img src='/public/img/sword.png'></img>Link 3</a>
+            <a href="#"><img src='/public/img/finish-flag.png'></img>Link 4</a>
             </div>
         </li>
     )
